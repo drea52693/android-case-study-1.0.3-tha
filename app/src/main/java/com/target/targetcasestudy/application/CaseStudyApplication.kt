b9183de886418129dd057faeffb9d06a33767f33
Dan Rea <drea2@mix.wvu.edu>
2024-06-26 20:54:24 -0400

    Added deal detail screen

---
+++ app/src/main/java/com/target/targetcasestudy/application/CaseStudyApplication.kt
@@ -0,0 +1,8 @@
+package com.target.targetcasestudy.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CaseStudyApplication: Application() {
+}
