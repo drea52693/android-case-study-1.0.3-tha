746d13cc1c5f3caa70b735997ecf36ac9fdc2416
Dan Rea <drea2@mix.wvu.edu>
2024-06-26 20:54:11 -0400

    Added font, strings, colors and images

---
+++ app/src/main/java/com/target/targetcasestudy/ui/Colors.kt
@@ -0,0 +1,12 @@
+package com.target.targetcasestudy.ui
+
+import androidx.compose.ui.graphics.Color
+
+val MyCustomRed = Color(0xFFAA0000)
+val MyCustomDarkGray = Color(0xFF333333)
+val MyCustomBlack = Color(0xFF000000)
+val MyCustomGreen = Color(0xFF008300)
+val MyCustomGray = Color(0xFF666666)
+val MyCustomLightGray = Color(0xFF888888)
+val MyCustomLightRed = Color(0xFFCC0000)
+val MyCustomWhite = Color(0xFFFFFFFF)
