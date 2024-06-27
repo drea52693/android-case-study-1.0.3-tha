d9eecd7671453c9381912cffaa81e8897200ce46
Dan Rea <dan.rea@team.grindr.com>
2024-06-27 13:48:07 -0400

    Made the button on the details page sticky

--- app/src/main/java/com/target/targetcasestudy/ui/details/DealDetailsScreen.kt
+++ app/src/main/java/com/target/targetcasestudy/ui/details/DealDetailsScreen.kt
@@ -1,8 +1,6 @@
 package com.target.targetcasestudy.ui.details
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -32,6 +30,7 @@ import com.target.targetcasestudy.ui.theme.MyCustomRed
 import com.target.targetcasestudy.ui.theme.MyCustomWhite
 import com.target.targetcasestudy.ui.theme.robotoFontFamily
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun ProductDetailsScreen(
     viewModel: ProductDetailsViewModel,
@@ -76,83 +75,86 @@ fun ProductDetails(
     modifier: Modifier,
     deal: Deal,
 ) {
-    Column(
-        modifier = modifier
-                .fillMaxSize()
-                .padding(8.dp)
-                .verticalScroll(state = rememberScrollState()),
-    ) {
-        Image(
-            painter = rememberImagePainter(deal.imageUrl),
-            contentDescription = stringResource(id = R.string.product_image_content_description),
-            modifier =
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+                .padding(bottom = 40.dp, start = 8.dp, end = 8.dp )
+        ) {
+            Image(
+                painter = rememberImagePainter(deal.imageUrl),
+                contentDescription = stringResource(id = R.string.product_image_content_description),
+                modifier =
                 Modifier
                     .fillMaxWidth()
                     .height(328.dp),
-            contentScale = ContentScale.Crop,
-        )
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(
-            text = deal.title,
-            fontFamily = robotoFontFamily,
-            fontSize = 18.sp,
-            color = MyCustomBlack,
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Row {
-            Text(
-                text = deal.salePrice?.displayString ?: stringResource(id = R.string.default_sale_price),
-                fontFamily = robotoFontFamily,
-                fontSize = 21.sp,
-                fontWeight = FontWeight.Bold,
-                color = MyCustomRed,
+                contentScale = ContentScale.Crop,
             )
-            Spacer(modifier = Modifier.width(8.dp))
+            Spacer(modifier = Modifier.height(4.dp))
             Text(
-                modifier = Modifier.align(Alignment.Bottom),
-                text =
-                    stringResource(id = R.string.reg) + " " + (
-                        deal.regularPrice?.displayString
-                            ?: stringResource(id = R.string.default_regular_price)
-                    ),
+                text = deal.title,
                 fontFamily = robotoFontFamily,
-                fontSize = 12.sp,
+                fontSize = 18.sp,
+                color = MyCustomBlack,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row {
+                Text(
+                    text = deal.salePrice?.displayString ?: stringResource(id = R.string.default_sale_price),
+                    fontFamily = robotoFontFamily,
+                    fontSize = 21.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MyCustomRed,
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(
+                    modifier = Modifier.align(Alignment.Bottom),
+                    text =
+                    stringResource(id = R.string.reg) + " " + (
+                            deal.regularPrice?.displayString
+                                ?: stringResource(id = R.string.default_regular_price)
+                            ),
+                    fontFamily = robotoFontFamily,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = MyCustomDarkGray,
+                )
+            }
+            Text(
+                text = deal.fulfillment,
+                fontSize = 14.sp,
+                fontFamily = robotoFontFamily,
                 fontWeight = FontWeight.Normal,
+                color = MyCustomGray,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Divider(color = MyCustomDarkGray, thickness = 1.dp)
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(
+                text = stringResource(id = R.string.product_details),
+                fontFamily = robotoFontFamily,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
                 color = MyCustomDarkGray,
             )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = deal.description,
+                fontFamily = robotoFontFamily,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                color = MyCustomLightGray,
+            )
+            Spacer(modifier = Modifier.height(16.dp))
         }
-        Text(
-            text = deal.fulfillment,
-            fontSize = 14.sp,
-            fontFamily = robotoFontFamily,
-            fontWeight = FontWeight.Normal,
-            color = MyCustomGray,
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Divider(color = MyCustomDarkGray, thickness = 1.dp)
-        Spacer(modifier = Modifier.height(24.dp))
-        Text(
-            text = stringResource(id = R.string.product_details),
-            fontFamily = robotoFontFamily,
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-            color = MyCustomDarkGray,
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = deal.description,
-            fontFamily = robotoFontFamily,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Normal,
-            color = MyCustomLightGray,
-        )
-        Spacer(modifier = Modifier.height(16.dp))
         Button(
             onClick = { /* Handle add to cart click */ },
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .height(44.dp),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .height(44.dp),
+                //.padding(8.dp),
             colors = ButtonDefaults.buttonColors(backgroundColor = MyCustomLightRed),
         ) {
             Text(
